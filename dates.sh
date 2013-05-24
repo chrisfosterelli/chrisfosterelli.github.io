97a85482858c2206747c893a3c1a55bc591247d2
Chris Foster <chris.james.foster@gmail.com>
2013-05-23 19:44:29 -0700

    Added script to fix publishing dates

---
+++ dates.sh
@@ -0,0 +1,4 @@
+touch -d "August 10 2012" discovering-explosive-productivity.md
+touch -d "October 11 2012" exploiting-filepickerio.md
+touch -d "August 17 2012" modular-rapid-application-development.md
+touch -d "August 03 2012" two-weeks-of-awesome.md
