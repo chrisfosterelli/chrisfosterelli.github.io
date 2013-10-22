4069c4367fcf84b81420de933c846c048c240fbb
Chris Foster <chris.james.foster@gmail.com>
2013-10-22 11:59:53 -0700

    Revert to old GA code

--- bbj.sh
+++ bbj.sh
@@ -158,13 +158,19 @@ get_srcfile_full_text() {
 # Prints the required google analytics code
 google_analytics() {
     if [ "$global_analytics" == "" ]; then return; fi
+
     echo "<script type=\"text/javascript\">
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', '$global_analytics');
-  ga('send', 'pageview');
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '$global_analytics']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 </script>"
 }
 
