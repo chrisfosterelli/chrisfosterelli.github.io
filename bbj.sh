c6ff55c9575aefc1c970fa10287037fb84751e4c
Chris Foster <chris.james.foster@gmail.com>
2014-11-18 22:26:24 -0800

    Remove thedaywefightback.js

--- bbj.sh
+++ bbj.sh
@@ -494,17 +494,6 @@ $html_header
 			$html_footer
 		</div>
 	</div>
-	<!-- thedaywefightback.js -->
-	<!--[if !(lte IE 8)]><!-->
-	<script type="text/javascript">
-	  (function(){
-	    var e = document.createElement('script'); e.type='text/javascript'; e.async = true;
-	    e.src = document.location.protocol + '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
-	  })();
-	</script>
-	<!--<![endif]-->
-	<!-- end thedaywefightback.js -->
 </body>
 </html>
 EOF
